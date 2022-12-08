a227d22131503d5ccaa2d085ea5e2e59485902d0
jcha0713 <joocha0713@gmail.com>
2022-12-08 14:56:57 -0800

    Take more options from Segmenter constructor
    - allow users to extend the CJK unicode range
    - Omit the `grapheme` option

--- lib/better-reading-time.ts
+++ lib/better-reading-time.ts
@@ -1,14 +1,24 @@
 import { Counter } from './counter.js'
 import { Segmenter } from './segmenter.js'
 
+type charRange = [lowerBound: number, upperBound: number]
+
 interface Options {
   lang?: string
   wpm?: number
   cpm?: number
 }
 
-export async function countReadingTime(text: string, options: Options = {}) {
-  const segmenter = new Segmenter(text, options?.lang ?? 'en')
+export async function countReadingTime(
+  text: string,
+  options: Options = {},
+  extendedCharRange?: charRange[],
+) {
+  const segmenter = new Segmenter(
+    text,
+    options?.lang ?? 'en',
+    extendedCharRange,
+  )
   const words = await segmenter.getWords()
   const counter = new Counter(words, options)
   const minutes = counter.getMinutes()
--- lib/segmenter.ts
+++ lib/segmenter.ts
@@ -2,31 +2,63 @@ import fs from 'node:fs'
 import { createIntlSegmenterPolyfill } from 'intl-segmenter-polyfill'
 // import wasmUrl from '../node_modules/intl-segmenter-polyfill/dist/break_iterator.wasm?url'
 
-type Options = {
-  granularity: 'word' | 'grapheme'
-}
+type charRange = [lowerBound: number, upperBound: number]
 
 interface Word {
-  segment: string
+  text: string
+  isCJK: boolean
 }
 
 export class Segmenter {
   public text: string
   public locale: string
-  public options: Options
+  public options: { granularity: 'word' | 'grapheme' }
+  public extendedCharRange: charRange[]
 
-  constructor(text: string, locale: string, options: Options) {
+  constructor(text: string, locale?: string, extendedCharRange?: charRange[]) {
     this.text = text
-    this.locale = locale
-    this.options = options
+    this.locale = locale ?? 'en'
+    this.options = { granularity: 'word' }
+    this.extendedCharRange = extendedCharRange ?? []
   }
 
   public async getWords(): Promise<Word[]> {
     const segments = await this.useSegmenter()
-    const words = segments.map((segmentItem) => {
-      return segmentItem.segment
+    const words = segments.map((segmentItem): Word => {
+      return {
+        text: segmentItem.segment,
+        isCJK: this.isCJK(segmentItem.segment),
+      }
     })
     return words
+  }
+
+  private isCJK(word: string): boolean {
+    const charCode = word.charCodeAt(0)
+    const CJKUnicodeRanges: charRange[] = [
+      // Hiragana
+      [0x30_41, 0x30_96],
+      // Katakana
+      [0x30_a0, 0x30_ff],
+      // CJK Unified ideographs
+      [0x4e_00, 0x9f_ff],
+      // Hangul
+      [0xac_00, 0xd7_a3],
+      // CJK extensions
+      [0x2_00_00, 0x2_eb_e0],
+    ]
+
+    this.extendedCharRange.forEach((range) => {
+      if (range[0] > range[1]) {
+        range.reverse()
+      }
+
+      CJKUnicodeRanges.push(range)
+    })
+
+    return CJKUnicodeRanges.some(
+      ([low, high]) => charCode >= low && charCode <= high,
+    )
   }
 
   private async useSegmenter() {
