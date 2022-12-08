3d9c3bc298232fe17280466fc1a12b3f676d8ec3
jcha0713 <joocha0713@gmail.com>
2022-12-08 15:00:19 -0800

    Implement the core word/char counting logic
    - for CJK chars, count the chars and use cpm
    - for non CJK words, count it as a word and use wpm

--- lib/counter.ts
+++ lib/counter.ts
@@ -1,5 +1,12 @@
 interface Word {
-  segment: string
+  text: string
+  isCJK: boolean
+}
+
+interface Options {
+  lang?: string
+  wpm?: number
+  cpm?: number
 }
 
 export class Counter {
@@ -8,7 +15,8 @@ export class Counter {
   public lang: string
   public words: Word[]
 
-  constructor(words: Word[], lang = 'en', wpm = 270, cpm = 500) {
+  constructor(words: Word[], options: Options = {}) {
+    const { lang = 'en', wpm = 270, cpm = 500 } = options
     this.words = words
     this.lang = lang
     this.wpm = wpm
@@ -16,10 +24,24 @@ export class Counter {
   }
 
   public getMinutes() {
-    return this.getPrettyNumber(this.words.length / this.wpm)
+    let chars = 0
+    for (const word of this.words) {
+      if (word.isCJK) {
+        chars += word.text.length
+      }
+    }
+
+    const CJKMinutes = chars / this.cpm
+    const nonCJKMinutes =
+      this.words.filter((word) => !word.isCJK).length / this.wpm
+
+    console.log('CJK:', CJKMinutes)
+    console.log('Non CJK:', nonCJKMinutes)
+
+    return this.getPrettyNumber(CJKMinutes + nonCJKMinutes)
   }
 
-  public getPrettyNumber(minutes: number) {
+  private getPrettyNumber(minutes: number) {
     const prettyNumber = Math.ceil(Number.parseFloat(minutes.toFixed(2)))
     return prettyNumber
   }
